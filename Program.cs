b81af033e254793bb49fbdcc8603788977b13d77
Riccardo Di Savino <riccardo.disavino@integratedsolutions.it>
2021-12-22 08:38:28 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationsTDPC13
+{
+  class Program
+  {
+    /*
+     
+     */
+    static void Main(string[] args)
+    {
+      string nome = "Ciccio";
+      Console.WriteLine(nome);
+
+      Console.WriteLine("Ciccio");
+
+      Console.ReadKey();
+    }
+  }
+}
