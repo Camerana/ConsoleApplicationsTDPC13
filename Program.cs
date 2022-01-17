e777bbbdf011960bddb4fa264afea04c73ff612b
Riccardo Di Savino <riccardo.disavino@integratedsolutions.it>
2022-01-17 21:18:28 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -45,23 +45,52 @@ namespace ConsoleApplicationsTDPC13
       Console.ReadKey();
       */
 
+      /*
+      bool valore1 = true;
+      bool valore2 = false;
+      Console.WriteLine(valore1);
+      Console.WriteLine(valore2);      
+      */
 
-      Console.WriteLine("int");
-      Console.WriteLine(int.MinValue);
-      Console.WriteLine(int.MaxValue);
+      /*
+      string inputDellUtente = Console.ReadLine();
+      Console.WriteLine(inputDellUtente);
+      */
 
-      Console.WriteLine("double");
-      Console.WriteLine(double.MinValue);
-      Console.WriteLine(double.MaxValue);
+      /*
+       scrivere un programma che:
+        - chieda all'utente il suo nome
+        - scriva sullo schermo "il tuo nome è + nome"
+       */
+      /*
+      Console.WriteLine("Come ti chiami ?");
+      string nome = Console.ReadLine();
+      Console.WriteLine("Il tuo nome è: " + nome);
+      */
 
-      Console.WriteLine("float");
-      Console.WriteLine(float.MinValue);
-      Console.WriteLine(float.MaxValue);
+      /*
+      string numeroString = "10";
+      int numeroInt = int.Parse(numeroString);      
+      Console.WriteLine(numeroString);
+      Console.WriteLine(numeroInt);
+      */
 
-      Console.WriteLine("UInt16");
-      Console.WriteLine(UInt16.MinValue);
-      Console.WriteLine(UInt16.MaxValue);
-
+      /*
+       scrivere un programma che:
+        - chieda all'utente due numeri (numero1 e numero2)
+        - scriva sullo schermo numero1 + 1 (somma aritmetica)
+        - scriva sullo schermo la somma dei due numeri
+        - scriva sullo schermo la differenza dei due numeri
+       */
+      Console.WriteLine("Inserisci il primo numero");
+      string numero1 = Console.ReadLine();
+      Console.WriteLine("Inserisci il secondo numero");
+      string numero2 = Console.ReadLine();
+      int numero1Int = int.Parse(numero1);
+      int numero2Int = int.Parse(numero2);
+      Console.WriteLine("numero1 + 1 = " + (numero1Int + 1));
+      Console.WriteLine("numero1 + numero2 = " + (numero1Int + numero2Int));
+      Console.WriteLine("numero1 - numero2 = " + (numero1Int - numero2Int));
       Console.ReadKey();
     }
   }
