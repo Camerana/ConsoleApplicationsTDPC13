2973bc1e0d0256eda4eb59574003d2f2fe5c4719
Riccardo Di Savino <riccardo.disavino@integratedsolutions.it>
2022-01-17 20:10:46 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -8,15 +8,59 @@ namespace ConsoleApplicationsTDPC13
 {
   class Program
   {
-    /*
-     
-     */
+
     static void Main(string[] args)
     {
-      string nome = "Ciccio";
+      /*
+      string nome = "Francesco";
       Console.WriteLine(nome);
 
-      Console.WriteLine("Ciccio");
+      Console.ReadKey();
+      */
+      /*
+      int numero = 12;
+      int numero2 = 1;
+      int somma = numero + numero2;
+      Console.WriteLine(somma);
+      Console.ReadKey();
+      */
+
+      /*
+       - scrivere un programma che:
+        - date due stringhe, ne stampi la concatenazione
+        - dati due numeri, ne stampi somma e differenza       
+       */
+      /*
+      string stringa1 = "questa è la stringa 1";
+      string stringa2 = "questa è la stringa 2";
+      Console.WriteLine(stringa1 + " " + stringa2);
+      Console.ReadKey();
+
+      int numero1 = 10;
+      int numero2 = 5;
+      Console.WriteLine(numero1 + numero2);
+      Console.WriteLine(numero1 - numero2);
+      Console.WriteLine("La somma dei numeri è: " + (numero1 + numero2));
+      Console.WriteLine("La differenza dei numeri è: " + (numero1 - numero2));
+      Console.ReadKey();
+      */
+
+
+      Console.WriteLine("int");
+      Console.WriteLine(int.MinValue);
+      Console.WriteLine(int.MaxValue);
+
+      Console.WriteLine("double");
+      Console.WriteLine(double.MinValue);
+      Console.WriteLine(double.MaxValue);
+
+      Console.WriteLine("float");
+      Console.WriteLine(float.MinValue);
+      Console.WriteLine(float.MaxValue);
+
+      Console.WriteLine("UInt16");
+      Console.WriteLine(UInt16.MinValue);
+      Console.WriteLine(UInt16.MaxValue);
 
       Console.ReadKey();
     }
