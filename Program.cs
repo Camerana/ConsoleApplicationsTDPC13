7fd8e6e135605b39ce1201b538540a823bc11f74
Riccardo Di Savino <riccardo.disavino@integratedsolutions.it>
2022-01-18 19:36:05 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace ConsoleApplicationsTDPC13
   {
     static void Main(string[] args)
     {
+      /*
       Console.WriteLine("Inserisci una stringa");
       string variabile = Console.ReadLine();
       if (variabile == "ciao")
@@ -28,6 +29,42 @@ namespace ConsoleApplicationsTDPC13
       {
         Console.WriteLine("non mi saluti ?");
       }
+      */
+      /*
+       scrivere un programma che:
+        - chieda all'utente una stringa
+        - confronti la stringa inserita con una stringa
+        arbitraria
+        - in caso di uguaglianza stampi su schermo
+        "le stringhe sono uguali"
+        - altrimenti stampi su schermo
+        "le stringhe non sono uguali"
+       */
+      /*
+      Console.WriteLine("inserisci una stringa");
+      string stringa = Console.ReadLine();
+      if (stringa == "stringa segreta")
+        Console.WriteLine("le stringhe sono uguali");
+      else
+        Console.WriteLine("le stringhe non sono uguali");
+      */
+      /*versione che gira fino a quando l'utente non 
+       indovina la stringa "asd"*/
+      /*
+      bool exitLoop = false;
+      while (!exitLoop)
+      {
+        Console.WriteLine("inserisci una stringa");
+        string stringa = Console.ReadLine();
+        if (stringa == "asd")
+        {
+          exitLoop = true;
+          Console.WriteLine("le stringhe sono uguali");
+        }
+        else
+          Console.WriteLine("le stringhe non sono uguali");
+      }
+      */
 
 
 
