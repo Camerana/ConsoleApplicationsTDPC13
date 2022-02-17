a55cf04a855f61ef43a671f71fe239cc2d1590a5
Riccardo Di Savino <riccardo.disavino@integratedsolutions.it>
2022-02-17 21:17:01 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -10,7 +10,25 @@ namespace ConsoleApplicationsTDPC13
   {
     static void Main(string[] args)
     {
-     
+      /*
+      scrivere un programma:
+        - chiedere all'utente 4 stringhe
+        - trovare tra le stringhe quella più lunga e stamparla su schermo
+        - trovare tra le stringhe quella più corta e stamparla su schermo
+
+      scrivere un programma:
+        - dati due numeri interi:
+          - stampare su schermo la somma e la differenza dei numeri
+          - stampare su schermo un rettangolo con dimensioni uguali ai due numeri
+
+      scrivere un programma:
+        - chiedere all'utente nome e cognome
+        - stampare su schermo il numero di caratteri del nome
+        - chiedere all'utente un numero
+        - stampare su schermo la somma tra il numero e il numero di caratteri del nome
+
+      scrivere un programma che simuli la battaglia navale in due giocatori
+       */
     }
   }
 }
