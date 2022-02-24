296b9cd1f72aa7227ef4383e1e21eef752949dd5
Riccardo Di Savino <riccardo.disavino@integratedsolutions.it>
2022-02-24 20:31:43 +0100

    asd

--- Program.cs
+++ Program.cs
@@ -6,11 +6,23 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplicationsTDPC13
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-     
+        static void Main(string[] args)
+        {
+            /*
+            List<string> stringhe = new List<string>() {
+            "lista 1", "lista 2", "lista 3"
+            };
+            foreach (string stringa in stringhe)
+                Console.WriteLine(stringa);
+
+            int[] numeri = new int[] { 1, 2, 3 };
+
+            foreach (int numero in numeri)
+                Console.WriteLine(numero);
+            */
+            Console.ReadKey();
+        }
     }
-  }
 }
