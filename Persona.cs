0c7ea642ab91393e20f033988914e895e1c5686d
Riccardo Di Savino <riccardo.disavino@integratedsolutions.it>
2022-03-15 20:53:56 +0100

    persona

---
+++ Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationsTDPC13
+{
+    class Persona
+    {
+        public string Name;
+        public int Eta;
+
+        public void StampaDati()
+        {
+            Console.WriteLine(this.Name);
+            Console.WriteLine(this.Eta);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,5 +12,38 @@ namespace ConsoleApplicationsTDPC13
     {
      
     }
-  }
+        /*
+    creare una classe Persona con properties:
+       - public string Name
+       - public int Eta
+    creare 2 oggetti di tipo Persona
+    stampare su schermo le property dei due oggetti
+    */
+        /*
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona();
+            Persona persona2 = new Persona();
+
+            persona1.Name = "Fabrizio";
+            persona1.Eta = 25;
+
+            persona2.Name = "Michele";
+            persona2.Eta = 60;
+
+
+            Console.WriteLine(persona1.Name);
+            Console.WriteLine(persona1.Eta);
+
+            Console.WriteLine(persona2.Name);
+            Console.WriteLine(persona2.Eta);
+
+            //questi hanno lo stesso output di sopra
+            persona1.StampaDati();
+            persona2.StampaDati();
+
+            Console.ReadKey();
+        }
+        */
+    }
 }
