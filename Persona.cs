386b7986efec67ee037818bdde09c19b8099f860
Riccardo Di Savino <riccardo.disavino@integratedsolutions.it>
2022-03-17 19:32:57 +0100

    property

---
+++ Persona.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationsTDPC13
+{
+    class Persona
+    {
+        private string nome;
+        public string Nome
+        {
+            get
+            {
+                return this.nome + " dal Get";
+            }
+            set
+            {
+                this.nome = value + " dal Set";
+            }
+        }
+        public string Cognome { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,11 +6,16 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplicationsTDPC13
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-     
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.Nome = "Franco";
+            Console.WriteLine(persona.Nome);
+            Console.ReadKey();
+
+            
+        }
     }
-  }
 }
