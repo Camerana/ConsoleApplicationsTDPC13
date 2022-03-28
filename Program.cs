960ffee11a86bd9fd9c546e1d4f9aeedfbeecbda
Riccardo Di Savino <riccardo.disavino@integratedsolutions.it>
2022-03-28 20:25:22 +0200

    asd

--- Program.cs
+++ Program.cs
@@ -6,11 +6,33 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplicationsTDPC13
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-     
+        static void Main(string[] args)
+        {
+            Automobile automobile = new Automobile();
+            automobile.Colore = "Rosso";
+            Sovrascrivi(automobile.Colore);
+            Sovrascrivi(automobile);
+
+
+            string coloreString = "Blu";
+            Sovrascrivi(coloreString);
+
+            Console.ReadKey();
+        }
+        /*
+         i tipi di base vengono passati per value,
+        gli oggetti vengono passati per reference !
+         */
+        public static void Sovrascrivi(string colore)
+        {
+            colore = "Giallo";
+        }
+        public static void Sovrascrivi(Automobile auto)
+        {
+            auto.Colore = "Verde";
+        }
     }
-  }
 }
+
