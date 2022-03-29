bc51fcfcea4b443f96447271c2bb4f8aaac5b905
Riccardo Di Savino <riccardo.disavino@integratedsolutions.it>
2022-03-29 20:38:04 +0200

    git

---
+++ Persona.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationsTDPC13
+{
+    public class Persona
+    {
+        private string nome;
+        public string Nome
+        {
+            get
+            {
+                return this.nome;
+            }
+            set
+            {
+                this.nome = value + "ggg";
+            }
+        }
+        public string Cognome { get; set; }
+        public int Eta { get; set; }
+
+        public Persona(string nome, string cognome)
+        {
+            this.Nome = nome;
+            this.Cognome = cognome;
+        }
+        public Persona(string nome, string cognome, bool saluta)
+        {
+            this.Nome = nome;
+            this.Cognome = cognome;
+
+            if (saluta)
+                Saluta(this.Nome);
+        }
+        public void Saluta(string nome)
+        {
+            Console.WriteLine("Ciao, sono " + nome);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,11 +6,31 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplicationsTDPC13
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-     
+        /*
+         creare una classe Animale con:
+            - properties:
+                - string Nome
+                - string Razza
+            - costruttore vuoto (di default)
+            - costruttore con parametro:
+                - string nome
+            - costruttore con parametri:
+                - string nome
+                - string razza
+            - metodo Muoviti()
+        creare un totale di 3 oggetti della classe Animale usando tutti e 3 
+        i costruttori, valorizzarne le properties e per ciascuno
+        eseguire il metodo Muoviti()
+         */
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona("Gino", "Paoli", true);
+            Persona persona2 = new Persona("Claudio", "Villa", false);
+            Persona persona3 = new Persona("Fred", "Bongusto", true);
+
+
+        }
     }
-  }
 }
