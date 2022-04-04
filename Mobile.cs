87f35a5f37a5edfdb728d0580c3761e9f485e171
Riccardo Di Savino <riccardo.disavino@integratedsolutions.it>
2022-04-04 19:14:16 +0200

    asd

---
+++ Mobile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationsTDPC13
+{
+    public class Mobile
+    {
+        public int Altezza { get; set; }
+        public string Colore { get; set; }
+
+        public void StampaMobile()
+        {
+            Console.WriteLine("Altezza: " + this.Altezza);
+            Console.WriteLine("Colore: " + this.Colore);
+        }
+    }
+    public class Tavolo : Mobile
+    {
+    }
+    public class Armadio : Mobile
+    {
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,11 +6,37 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplicationsTDPC13
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-     
+        /*
+            - creare una classe Animale
+                - con property string TipologiaArti
+                - con metodo Mangia
+            - creare una classe Elefante : Animale
+                - con metodo Corri
+            - creare una classe Pesce : Animale
+                - con metodo Nuota
+            - istanziare un oggetto di tipo Elefante
+                - popolarne le properties
+                - invocare i metodi Mangia e Corri
+            - istanziare un oggetto di tipo Pesce
+                - popolarne le properties
+                - invocare i metodi Mangia e Nuota
+         */
+        static void Main(string[] args)
+        {
+            Tavolo tavolo = new Tavolo();
+            tavolo.Altezza = 100;
+            tavolo.Colore = "Rosso";
+
+            Armadio armadio = new Armadio();
+            armadio.Altezza = 200;
+            armadio.Colore = "Nero";
+
+            tavolo.StampaMobile();
+            armadio.StampaMobile();
+
+            Console.ReadKey();
+        }
     }
-  }
 }
