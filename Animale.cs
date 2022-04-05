0f57a6ffee661df3ed89e2074d17f929260396f0
Riccardo Di Savino <riccardo.disavino@integratedsolutions.it>
2022-04-05 20:32:14 +0200

    asd

---
+++ Animale.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationsTDPC13
+{
+    public abstract class Animale
+    {
+        public void Mangia() { }
+        public virtual void Muoviti() { }
+    }
+    public class Pesce : Animale
+    {
+        public override void Muoviti() { }
+    }
+    public class Gatto : Animale
+    {
+        public override void Muoviti() { }
+    }
+    public class Canarino : Animale
+    {
+        public override void Muoviti() { }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,11 +6,48 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplicationsTDPC13
 {
-  class Program
-  {
-    static void Main(string[] args)
+
+    class Program
     {
-     
+        /*
+         creare una classe astratta Vegetale
+            - con property:
+                - Altezza
+            - con metodi:
+                - Fiorisci
+        - creare una classe astratta Albero : Vegetale
+            - con property:
+                - TipologiaLegno
+                - TipologiaFrutto
+        - creare una classe Platano : Albero
+            - con metodi:
+                - ProduciBanane()
+        - creare una classe Ulivo : Albero
+            - con metodi:
+                - ProduciOlive()
+        - creare una classe Quercia : Albero
+            - con metodi:
+                - ViviTanto()
+        - creare 10 oggetti di tipo Platano
+        - creare un oggetto di tipo Ulivo
+        - creare un oggetto di tipo Quercia
+        - popolare le property degli oggetti
+        - invocare da Program i metodi di tutti gli oggetti creati
+         */
+        static void Main(string[] args)
+        {
+            Pesce ps = new Pesce();
+            Gatto gt = new Gatto();
+            Canarino cn = new Canarino();
+
+            Metodo(ps);
+            Metodo(gt);
+            Metodo(cn);
+        }
+        static void Metodo(Animale animale)
+        {
+            animale.Mangia();
+            animale.Muoviti();
+        }
     }
-  }
 }
