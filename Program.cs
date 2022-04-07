585d1766fbc13e5bf6137bad2616f9481987afda
Riccardo Di Savino <riccardo.disavino@integratedsolutions.it>
2022-04-07 18:57:17 +0200

    asd

--- Program.cs
+++ Program.cs
@@ -36,6 +36,46 @@ namespace ConsoleApplicationsTDPC13
          */
         static void Main(string[] args)
         {
+            for (int i = 0; i < 10; i++)
+            {
+                Platano pl = new Platano();
+                pl.Altezza = 150;
+                pl.TipologiaFrutto = "tipo frutto platano";
+                pl.TipologiaLegno = "tipo legno platano";
+                Console.WriteLine(pl.Altezza);
+                Console.WriteLine(pl.TipologiaFrutto);
+                Console.WriteLine(pl.TipologiaLegno);
+                pl.Fiorisci();
+                pl.ProduciBanane();
+            }
+
+            Ulivo ul = new Ulivo();
+            ul.Altezza = 170;
+            ul.TipologiaFrutto = "tipo frutto ulivo";
+            ul.TipologiaLegno = "tipo legno ulivo";
+            Console.WriteLine(ul.Altezza);
+            Console.WriteLine(ul.TipologiaFrutto);
+            Console.WriteLine(ul.TipologiaLegno);
+            ul.Fiorisci();
+            ul.ProduciOlive();
+
+            for (int i = 0; i < 15; i++)
+            {
+                Quercia q = new Quercia();
+                q.Altezza = 150;
+                q.TipologiaFrutto = "tipo frutto quercia";
+                q.TipologiaLegno = "tipo legno quercia";
+                Console.WriteLine(q.Altezza);
+                Console.WriteLine(q.TipologiaFrutto);
+                Console.WriteLine(q.TipologiaLegno);
+                q.Fiorisci();
+                q.ViviTanto();
+            }
+            Console.ReadKey();
+        }
+        /*
+        static void Main(string[] args)
+        {
             Pesce ps = new Pesce();
             Gatto gt = new Gatto();
             Canarino cn = new Canarino();
@@ -49,5 +89,6 @@ namespace ConsoleApplicationsTDPC13
             animale.Mangia();
             animale.Muoviti();
         }
+        */
     }
 }
---
+++ Vegetale.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationsTDPC13
+{
+    public abstract class Vegetale
+    {
+        public int Altezza { get; set; }
+        public void Fiorisci() { }
+    }
+    public abstract class Albero : Vegetale
+    {
+        public string TipologiaLegno { get; set; }
+        public string TipologiaFrutto { get; set; }
+    }
+    public class Platano : Albero
+    {
+        public void ProduciBanane()
+        {
+            Console.WriteLine("Il platano sta producendo banane");
+        }
+    }
+    public class Ulivo : Albero
+    {
+        public void ProduciOlive()
+        {
+            Console.WriteLine("L'ulivo sta producendo olive");
+        }
+    }
+    public class Quercia : Albero
+    {
+        public void ViviTanto()
+        {
+            Console.WriteLine("La quercia vive a lungo");
+        }
+    }
+}
