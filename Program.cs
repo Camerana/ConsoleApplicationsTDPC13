86327e7d4f2c57480f534c122c92b002be6ceecf
Riccardo Di Savino <riccardo.disavino@integratedsolutions.it>
2022-04-26 19:48:44 +0200

    asd

--- Program.cs
+++ Program.cs
@@ -12,21 +12,21 @@ namespace ConsoleApplicationsTDPC13
          Gara di salsicce e birre:
             - regole:
                 - 2 giocatori simulati
-                - randomizzare un punteggio arbitrario di punti "sforzo"
+                - randomizzare un punteggio arbitrario di punti "panza"
                 per i due giocatori
                 - ogni giocatore deve ad ogni turno tentare di mangiare
                 una salsiccia o bere una birra
-                - mangiare una salsiccia richiede uno "sforzo" di 3, però
+                - mangiare una salsiccia richiede uno "panza" di 3, però
                 sottrae anche 2 punti all'avversario
-                - bere una birra richiede uno "sforzo" di 2
+                - bere una birra richiede uno "panza" di 2
                 - ogni volta che si sceglie se mangiare o bere, randomizzare
                 un numero che indica se l'azione è andata a buon fine o meno
                     - se non va a buon fine, si subisce una penalità di 2 punti
                     per la birra, 3 punti per la salsiccia
                 - si vince se:
                     - alla fine dei turni, si è conservato il maggior 
-                    numero di punti "sforzo"
-                    - l'avversario scende a 0 punti "sforzo"
+                    numero di punti "panza"
+                    - l'avversario scende a 0 punti "panza"
          */
         static void Main(string[] args)
         {
