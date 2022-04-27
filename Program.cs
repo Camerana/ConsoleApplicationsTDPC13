17884a7e7b52bace166c7edee43eeea093c199a6
Riccardo Di Savino <riccardo.disavino@integratedsolutions.it>
2022-04-27 22:26:42 +0200

    a culo

--- Program.cs
+++ Program.cs
@@ -18,7 +18,46 @@ namespace ConsoleApplicationsTDPC13
          */
         static void Main(string[] args)
         {
-
+            Rubrica rubrica = new Rubrica();
+            while (true)
+            {
+                Console.WriteLine("Cosa vuoi fare ?");
+                Console.WriteLine("0) Aggiungi contatto");
+                Console.WriteLine("1) Stampa tutti i contatti");
+                Console.WriteLine("2) Cerca contatti");
+                Console.WriteLine("3) Modifica contatto");
+                Console.WriteLine("4) Elimina contatto");
+                string input = Console.ReadLine();
+                Console.Clear();
+                if (input == "0")
+                {
+                    rubrica.AggiungiContatto();
+                    Console.Clear();
+                }
+                else if (input == "1")
+                {
+                    rubrica.StampaContatti();
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else if (input == "2")
+                {
+                    rubrica.CercaContatti();
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else if (input == "3")
+                {
+                    rubrica.ModificaContatto();
+                    Console.Clear();
+                }
+                else if (input == "4")
+                {
+                    rubrica.CancellaContatto();
+                    Console.Clear();
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
---
+++ Rubrica.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplicationsTDPC13
+{
+    public class Rubrica
+    {
+        public List<Contatto> Contatti { get; set; } = new List<Contatto>();
+
+        public void AggiungiContatto()
+        {
+            Contatto contatto = new Contatto();
+            Console.WriteLine("Inserisci il nome:");
+            contatto.Nome = Console.ReadLine();
+            Console.WriteLine("Inserisci il cognome:");
+            contatto.Cognome = Console.ReadLine();
+            Console.WriteLine("Inserisci il numero di telefono:");
+            contatto.Telefono = Console.ReadLine();
+            this.Contatti.Add(contatto);
+        }
+
+        public void StampaContatti(List<Contatto> contatti = null)
+        {
+            List<Contatto> daStampare = null;
+            if (contatti != null)
+                daStampare = contatti;
+            else
+                daStampare = this.Contatti;
+            for (int i = 0; i < daStampare.Count; i++)
+            {
+                Console.WriteLine(i + ")" + daStampare[i].Nome + " " + daStampare[i].Cognome + " " + daStampare[i].Telefono);
+            }
+        }
+
+        public List<Contatto> CercaContatti()
+        {
+            Console.WriteLine("Inserisci il criterio:");
+            string input = Console.ReadLine();
+            List<Contatto> contattiTrovati = this.Contatti.Where(c =>
+            c.Nome.Contains(input)
+            || c.Cognome.Contains(input)
+            || c.Telefono.Contains(input)).ToList();
+            this.StampaContatti(contattiTrovati);
+            return contattiTrovati;
+        }
+
+        public void CancellaContatto()
+        {
+            Console.WriteLine("Quale contatto vuoi eliminare ?");
+            this.StampaContatti();
+            string input = Console.ReadLine();
+            int inputInt = 0;
+            if (int.TryParse(input, out inputInt))
+            {
+                if (inputInt >= 0 && inputInt < this.Contatti.Count)
+                    this.Contatti.RemoveAt(inputInt);
+            }
+        }
+
+        public void ModificaContatto()
+        {
+            Console.WriteLine("Quale contatto vuoi modificare ?");
+            this.StampaContatti();
+            string input = Console.ReadLine();
+            int inputInt = 0;
+            if (int.TryParse(input, out inputInt))
+            {
+                if (inputInt >= 0 && inputInt < this.Contatti.Count)
+                {
+                    Contatto contatto = new Contatto();
+                    Console.WriteLine("Inserisci il nome:");
+                    contatto.Nome = Console.ReadLine();
+                    Console.WriteLine("Inserisci il cognome:");
+                    contatto.Cognome = Console.ReadLine();
+                    Console.WriteLine("Inserisci il numero di telefono:");
+                    contatto.Telefono = Console.ReadLine();
+                    this.Contatti.RemoveAt(inputInt);
+                    this.Contatti.Insert(inputInt, contatto);
+                }
+            }
+        }
+    }
+
+
+    public class Contatto
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public string Telefono { get; set; }
+    }
+
+
+}
+
